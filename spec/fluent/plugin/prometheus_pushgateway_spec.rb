6b20f125083ba8d5d8c1cc99a39cf75c39a2b85c
Yuta Iwama <ganmacs@gmail.com>
2020-02-13 12:21:46 +0900

    for rspec setting
    Signed-off-by: Yuta Iwama <ganmacs@gmail.com>

--- spec/fluent/plugin/prometheus_pushgateway_spec.rb
+++
@@ -1,9 +0,0 @@
-RSpec.describe Fluent::Plugin::PrometheusPushgateway do
-  it "has a version number" do
-    expect(Fluent::Plugin::PrometheusPushgateway::VERSION).not_to be nil
-  end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
-end
