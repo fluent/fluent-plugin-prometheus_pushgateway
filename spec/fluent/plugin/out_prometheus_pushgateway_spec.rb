a27c387c8e8ad8195d6cb23e008b5c904fba8eda
Yuta Iwama <ganmacs@gmail.com>
2020-02-18 18:26:12 +0900

    Add some specs
    Signed-off-by: Yuta Iwama <ganmacs@gmail.com>

--- spec/fluent/plugin/out_prometheus_pushgateway_spec.rb
+++ spec/fluent/plugin/out_prometheus_pushgateway_spec.rb
@@ -17,5 +17,78 @@ RSpec.describe Fluent::Plugin::PrometheusPushgatewayOutput do
       driver = Fluent::Test::Driver::Output.new(described_class)
       expect { driver.configure(config) }.to raise_error(Fluent::ConfigError)
     end
+
+    context 'when endpoint is HTTPS' do
+      it 'returns default parameters' do
+        config = %[
+          @type prometheus_pushgateway
+          gateway https://127.0.0.1
+          job_name name
+        ]
+
+        driver = Fluent::Test::Driver::Output.new(described_class)
+        expect(driver.instance).to receive(:tls_options).and_wrap_original do |v|
+          r = v.call
+          expect(r).to eq({ verify_mode: OpenSSL::SSL::VERIFY_PEER, ciphers: 'ALL:!aNULL:!eNULL:!SSLv2', ssl_version: :TLSv1_2 })
+          r
+        end
+
+        expect { driver.configure(config) }.not_to raise_error
+      end
+
+      it 'sets verify_mode' do
+        config = %[
+          @type prometheus_pushgateway
+          gateway https://127.0.0.1
+          job_name name
+          tls_verify_mode none
+        ]
+
+        driver = Fluent::Test::Driver::Output.new(described_class)
+        expect(driver.instance).to receive(:tls_options).and_wrap_original do |v|
+          r = v.call
+          expect(r).to eq({ verify_mode: OpenSSL::SSL::VERIFY_NONE, ciphers: 'ALL:!aNULL:!eNULL:!SSLv2', ssl_version: :TLSv1_2 })
+          r
+        end
+
+        expect { driver.configure(config) }.not_to raise_error
+      end
+
+      it 'raises an ConfigError when tls_ca_cert_path is wrong' do
+        config = %[
+          @type prometheus_pushgateway
+          gateway https://127.0.0.1
+          job_name name
+          tls_ca_cert_path invalid
+        ]
+
+        driver = Fluent::Test::Driver::Output.new(described_class)
+        expect { driver.configure(config) }.to raise_error(Fluent::ConfigError, /tls_ca_cert_path is wrong/)
+      end
+
+      it 'raises an ConfigError when tls_client_cert_path is wrong' do
+        config = %[
+          @type prometheus_pushgateway
+          gateway https://127.0.0.1
+          job_name name
+          tls_client_cert_path invalid
+        ]
+
+        driver = Fluent::Test::Driver::Output.new(described_class)
+        expect { driver.configure(config) }.to raise_error(Fluent::ConfigError, /tls_client_cert_path is wrong/)
+      end
+
+      it 'raises an ConfigError when tls_private_key_path is wrong' do
+        config = %[
+          @type prometheus_pushgateway
+          gateway https://127.0.0.1
+          job_name name
+          tls_private_key_path invalid
+        ]
+
+        driver = Fluent::Test::Driver::Output.new(described_class)
+        expect { driver.configure(config) }.to raise_error(Fluent::ConfigError, /tls_private_key_path is wrong/)
+      end
+    end
   end
 end
